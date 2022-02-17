baf2f0947c39b79e3201caf4b820f8e069828913
unknown <tsertic5@gmail.com>
2022-02-17 23:47:20 +0100

    started hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import {
+  Section,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Button from "../../styles/GlobalComponents/Button";
+import { LeftSection } from "./HeroStyles";
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText></SectionText>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
